976ac1f82f6bb8ac22f708a372a97a36e6adc192
Stephen Benjamin <sbenjamin01@g.harvard.edu>
2017-08-22 19:07:42 -0400

    Add Java version of FixThisProgram

---
+++ fix-this-program/java/FixThisProgram.java
@@ -0,0 +1,11 @@
+class FixThisProgram {
+
+    public static void main(String args) {
+        System.out.println("Hello, world!");
+
+        for(int i = 0; i < 10; i++) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+}
