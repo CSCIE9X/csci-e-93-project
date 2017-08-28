885fd0e37528b0de24cb1d7b6658e9ace2e3a09d
Stephen Benjamin <sbenjamin01@g.harvard.edu>
2017-08-28 10:53:04 -0400

    Update C++ fix-this-program to be different than C version

--- fix-this-program/c++/fix-this-program.cpp
+++ fix-this-program/c++/fix-this-program.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
-void main(void) {
+int main(void) {
     std::cout << "hello, world\n";
-
-    for (int i = 0; i < 10; i++) {
-        std::cout << i;
-    }
-
+    print_integers(0, 10);
     std::cout << "\n";
+}
+
+void print_integers(int from, int to) {
+    for(int i = from; i < to; i++)
+        std::cout << i << " ";
 }
