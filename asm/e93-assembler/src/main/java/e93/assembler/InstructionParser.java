5027ab3486764d3b1cf0def40cdaa679cbac2265
Mark Ford <mark@massfords.com>
2020-04-10 09:06:44 -0400

    - move register/immediate methods to source match

--- asm/e93-assembler/src/main/java/e93/assembler/InstructionParser.java
+++ asm/e93-assembler/src/main/java/e93/assembler/InstructionParser.java
@@ -50,6 +50,25 @@ public class InstructionParser {
     static class SourceMatch {
         SourcePattern sourcePattern;
         Matcher matcher;
+
+        int getRegister(int group) {
+            String s = getMatcher().group(group);
+            try {
+                return Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                throw new InvalidRegisterException(s);
+            }
+        }
+
+        int getImmediate(int group) {
+            String s = getMatcher().group(group);
+            if (s.startsWith("0x")) {
+                return Integer.parseInt(s.substring(2), 16);
+            } else {
+                return Integer.parseInt(s);
+            }
+        }
+
     }
 
     @AllArgsConstructor
@@ -107,18 +126,14 @@ public class InstructionParser {
      */
     public static Instruction parse(String rawLine) {
         String line;
-        String comment; // todo could use this somewhere
         if (rawLine.contains("--")) {
             String[] lineAndComment = rawLine.split("--");
             line = lineAndComment[0];
-            comment = lineAndComment[1];
         } else {
             line = rawLine;
-            comment = null;
         }
 
         try {
-
             return Stream.of(Directives.values(), Instructions.values())
                     .flatMap(Arrays::stream)
                     .map(sourceMatcher -> sourceMatcher.matches(line))
@@ -128,17 +143,28 @@ public class InstructionParser {
                         if (sourceMatch.getSourcePattern() == Directives.ASCIIZ) {
                             return new Asciiz(sourceMatch.getMatcher().group(1));
                         } else if (sourceMatch.getSourcePattern() == Instructions.AND) {
-                            return new And().setR1(toRegister(sourceMatch.getMatcher().group(1))).setR2(toRegister(sourceMatch.getMatcher().group(2)));
+                            return new And()
+                                    .setR1(sourceMatch.getRegister(1))
+                                    .setR2(sourceMatch.getRegister(2));
                         } else if (sourceMatch.getSourcePattern() == Instructions.ADDI) {
-                            return new AddImmediate().setR1(toRegister(sourceMatch.getMatcher().group(1))).setImmediate(toImmediate(sourceMatch.getMatcher().group(2)));
+                            return new AddImmediate()
+                                    .setR1(sourceMatch.getRegister(1))
+                                    .setImmediate(sourceMatch.getImmediate(2));
                         } else if (sourceMatch.getSourcePattern() == Instructions.SW) {
-                            return new StoreWord().setR1(toRegister(sourceMatch.getMatcher().group(1))).setR2(toRegister(sourceMatch.getMatcher().group(2)));
+                            return new StoreWord()
+                                    .setR1(sourceMatch.getRegister(1))
+                                    .setR2(sourceMatch.getRegister(2));
                         } else if (sourceMatch.getSourcePattern() == Instructions.LW) {
-                            return new LoadWord().setR1(toRegister(sourceMatch.getMatcher().group(1))).setR2(toRegister(sourceMatch.getMatcher().group(2)));
+                            return new LoadWord()
+                                    .setR1(sourceMatch.getRegister(1))
+                                    .setR2(sourceMatch.getRegister(2));
                         } else if (sourceMatch.getSourcePattern() == Instructions.JUMP) {
-                            return new JumpImmediate().setImmediate(toImmediate(sourceMatch.getMatcher().group(1))<<1);
+                            return new JumpImmediate()
+                                    .setImmediate(sourceMatch.getImmediate(1)<<1);
                         } else if (sourceMatch.getSourcePattern() == Instructions.ORI) {
-                            return new OrImmediate().setR1(toRegister(sourceMatch.getMatcher().group(1))).setImmediate(toImmediate(sourceMatch.getMatcher().group(2)));
+                            return new OrImmediate()
+                                    .setR1(sourceMatch.getRegister(1))
+                                    .setImmediate(sourceMatch.getImmediate(2));
                         } else {
                             return new ErrorLine("unhandled instruction:" + sourceMatch.getMatcher().group(0));
                         }
@@ -148,47 +174,6 @@ public class InstructionParser {
                     .orElseGet(() -> new ErrorLine("Unknown instruction: " + line));
         } catch (InvalidRegisterException e) {
             return new ErrorLine("Invalid register " + e.getValue());
-        }
-    }
-
-    /**
-     * Helper method that converts a reference to a register to an int
-     *
-     * @param s reference to a register in the format we're expecting
-     * @return number of the register which is within range
-     * @throws IllegalArgumentException if we can't parse it or it's out of range
-     */
-    private static int toRegister(String s) throws InvalidRegisterException {
-        try {
-            return Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            throw new InvalidRegisterException(s);
-        }
-    }
-
-    /**
-     * Helper method that converts an immediate value to an integer
-     *
-     * @param s string version of a decimal number to use as an immediate
-     * @return converted integer
-     * @throws NumberFormatException if an unknown format
-     */
-    private static int toImmediate(String s) {
-        // todo - assert the value can be encoded
-
-        // With respect to the encoding assertion, keep in mind that you may only
-        // have 8 bits for an immediate value in your instruction (more for a
-        // JUMP). You may allow the programmer to use an immediate value outside
-        // of this range but that requires your assembler to handle it. For
-        // example, the assembler might see a large immediate value and then
-        // generate a couple of instructions to handle it. It might use a
-        // temporary register and LUI and similar instructions to put the large
-        // immediate into the temporary register and then rewrite the instruction
-        // to use this temporary register instead of an immediate.
-        if (s.startsWith("0x")) {
-            return Integer.parseInt(s.substring(2), 16);
-        } else {
-            return Integer.parseInt(s);
         }
     }
 }
